9ade4f59008745d9bedaaee1b5119f953197f428
FuzzyBootsDk <FuzzyBootsdk@icloud.com>
2026-02-20 12:22:38 +0100

    Update static assets: update hashed filenames and cache policies
    - Updated CSS asset fingerprints and ETag values.
    - Modified cache policies: set cache-control to `max-age=31536000, immutable`.
    - Regenerated build-related metadata and assembly information.

--- FilamentTracker/obj/Debug/net8.0/FilamentTracker.AssemblyInfo.cs
+++ FilamentTracker/obj/Debug/net8.0/FilamentTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilamentTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61ef6a3a5e257d3979fa8dd73d40fe856ed7f1db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8d2e71d61ce5e0fe1ed5f1605683b0bb349b20f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilamentTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilamentTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
